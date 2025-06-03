b4124490679ed8ba74a568da7bb200d8c3baee1f
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2025-06-04 00:23:57 +0200

    Added env types

--- FE/vehicles-dashboard/src/vite-env.d.ts
+++ FE/vehicles-dashboard/src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_SSL: string;
+  readonly VITE_ENABLE_HTTPS: boolean;
+  readonly VITE_SSL_KEY: string;
+  readonly VITE_SSL_CERT: string;
+  // add more as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
