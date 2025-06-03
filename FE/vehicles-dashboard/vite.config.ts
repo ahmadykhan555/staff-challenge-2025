81e136328642f9c9160b67ad20b6f0e5312a2125
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2025-06-04 00:23:49 +0200

    Set up environment based vite config

--- FE/vehicles-dashboard/vite.config.ts
+++ FE/vehicles-dashboard/vite.config.ts
@@ -1,17 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv, type ServerOptions } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig(() => {
+const DEFAULT_PORT = 3000;
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  console.log(env);
+  const serverConfig: ServerOptions = {
+    open: true,
+    port: typeof env.VITE_FE_PORT === 'string' ? JSON.parse(env.VITE_FE_PORT) : DEFAULT_PORT,
+    host: env.VITE_BASE_URL,
+  };
+
+  if (JSON.parse(env.VITE_ENABLE_HTTPS || '')) {
+    (serverConfig.https as Record<string, string>).key = env.VITE_SSL_KEY;
+    (serverConfig.https as Record<string, string>).cert = env.VITE_SSL_CERT;
+  }
+
   return {
     plugins: [react()],
     server: {
-      https: {
-        key: '../../cert/localhost-key.pem',
-        cert: '../../cert/localhost.pem',
-      },
-      host: 'localhost',
-      port: 3000,
+      ...serverConfig,
     },
   };
 });
