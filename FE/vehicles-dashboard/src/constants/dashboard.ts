80734b26d94e53d78e54970a131a86bcb9a7c7bf
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2025-06-07 01:10:45 +0200

    Moved fixed columns to constant

---
+++ FE/vehicles-dashboard/src/constants/dashboard.ts
@@ -0,0 +1,10 @@
+export const DEFAULT_VEHICLE_LISTING_COLUMNS = [
+  { displayName: 'address', id: 'address' },
+  { displayName: 'coordinates', id: 'coordinates' },
+  { displayName: 'engineType', id: 'engineType' },
+  { displayName: 'condition', id: 'condition' },
+  { displayName: 'fuel', id: 'fuel' },
+  { displayName: 'state', id: 'state' },
+  { displayName: 'licencePlate', id: 'licencePlate', sortById: 'licencePlate' },
+  { displayName: 'id', id: 'id' },
+];
