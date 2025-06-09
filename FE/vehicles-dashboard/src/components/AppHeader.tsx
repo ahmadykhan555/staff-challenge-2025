881b427e220a116869da7088d8c43fa05263cf85
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2025-06-09 22:35:55 +0200

    Minor improvements

--- FE/vehicles-dashboard/src/components/AppHeader.tsx
+++ FE/vehicles-dashboard/src/components/AppHeader.tsx
@@ -5,23 +5,19 @@ import Logo from '../assets/icons/logo.svg';
 const AppHeader = () => {
   const items = [
     {
-      to: '/',
-      displayName: 'home',
-    },
-    {
       to: '/dashboard',
       displayName: 'Dashboard',
     },
   ];
   return (
     <header className="flex border-b shadow-sm items-center">
-      <Link to={ROUTES.home} className=" px-4 py-3">
+      <Link to={ROUTES.home} className="px-4 ">
         <img src={Logo} />
       </Link>
       {items.map((item) => (
         <Link
           key={item.to}
-          className="border-r md:px-8 px-4 py-3 uppercase font-semibold"
+          className="border-r md:px-8 px-4 py-3 uppercase font-semibold last-of-type:border-0"
           to={item.to}
         >
           {item.displayName}
--- FE/vehicles-dashboard/src/constants/dashboard.ts
+++ FE/vehicles-dashboard/src/constants/dashboard.ts
@@ -3,9 +3,9 @@ export const DEFAULT_VEHICLE_LISTING_COLUMNS = [
   { displayName: 'condition', id: 'condition' },
   { displayName: 'engineType', id: 'engineType' },
   { displayName: 'fuel', id: 'fuel' },
-  { displayName: 'id', id: 'id' },
+  { displayName: 'id', id: 'id', sortById: 'id' },
   { displayName: 'licencePlate', id: 'licencePlate', sortById: 'licencePlate' },
-  { displayName: 'state', id: 'state' },
+  { displayName: 'state', id: 'state', sortById: 'state' },
   { displayName: 'coordinates', id: 'coordinates' },
   { displayName: 'type', id: 'carType' },
 ];
